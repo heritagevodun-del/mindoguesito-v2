5c65ece7a210232dc010dbba7f2e27abe41ffb3b
Heritage Vodun <heritagevodun@gmail.com>
2026-01-17 09:02:36 +0100

    feat(blog): Add new article 'Mindoguesito Genesis' & new category Innovation

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -18,7 +18,7 @@ export default function sitemap(): MetadataRoute.Sitemap {
       priority: 0.8,
     },
     {
-      url: `${baseUrl}/a-propos`,
+      url: `${baseUrl}/fonctionnalites`,
       lastModified: new Date(),
       changeFrequency: "monthly",
       priority: 0.5,
